06298e27c2fe2b74d0a0c8da10d8f0edd52627ee
suhwoo <rbfrnltls@gamil.com>
2020-04-12 14:40:16 +0900

    list 완성

--- app/src/main/java/com/example/myapplication/ListActivity.java
+++ app/src/main/java/com/example/myapplication/ListActivity.java
@@ -1,14 +1,51 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
-public class ListActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+import data.BoardData;
+
+public class ListActivity extends AppCompatActivity {
+    RecyclerView rv;//
+    ArrayList<BoardData> datas = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        rv = findViewById(R.id.list_rv);
+        setData();
+        setRv();
+    }
+
+    private void setRv() {
+        RecyclerView.LayoutManager lm = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
+        BoardAdapter adapter = new BoardAdapter(this,datas);
+
+        rv.setLayoutManager(lm);
+        rv.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void setData() {
+        /*
+        * String title;
+        String writer;
+        String date;
+        String readcomment;
+        * */
+        datas.clear();
+        datas.add(new BoardData("청산별곡1","박지원","1621.03.21","조회수 23 댓글 2"));
+        datas.add(new BoardData("청산별곡2","박지원","1621.03.21","조회수 23 댓글 2"));
+        datas.add(new BoardData("청산별곡3","박지원","1621.03.21","조회수 23 댓글 2"));
+        datas.add(new BoardData("청산별곡4","박지원","1621.03.21","조회수 23 댓글 2"));
+        datas.add(new BoardData("청산별곡5","박지원","1621.03.21","조회수 23 댓글 2"));
+        datas.add(new BoardData("청산별곡6","박지원","1621.03.21","조회수 23 댓글 2"));
+
+
     }
 }
