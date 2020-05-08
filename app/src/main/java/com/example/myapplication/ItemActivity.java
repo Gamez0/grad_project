9352c2eb5299b09b1e6e8ca607656d357c13dc9c
dobin <dobinshin@gmail.com>
2020-05-08 16:43:27 +0900

    init commit

--- app/src/main/java/com/example/myapplication/ItemActivity.java
+++ app/src/main/java/com/example/myapplication/ItemActivity.java
@@ -3,12 +3,26 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import dialog.ReviewDialog;
 
 public class ItemActivity extends AppCompatActivity {
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
+        fab = findViewById(R.id.item_edit);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ReviewDialog dialog = new ReviewDialog(ItemActivity.this);
+                dialog.show();
+            }
+        });
     }
 }
