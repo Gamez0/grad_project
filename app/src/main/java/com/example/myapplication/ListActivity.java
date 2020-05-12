a3d9e3450eae4f763eab69cd1ec22d446ba2f816
suhwoo <rbfrnltls@gamil.com>
2020-05-12 17:25:54 +0900

    해봅니다 commit... push도 해봅니다..

--- app/src/main/java/com/example/myapplication/ListActivity.java
+++ app/src/main/java/com/example/myapplication/ListActivity.java
@@ -42,7 +42,7 @@ public class ListActivity extends AppCompatActivity {
         datas.add(new BoardData("git연습","박지원","1621.03.21","조회수 23 댓글 2"));
         datas.add(new BoardData("이번엔 push까지","박지원","1621.03.21","조회수 23 댓글 2"));
         datas.add(new BoardData("push실패..","박지원","1621.03.21","조회수 23 댓글 2"));
-        datas.add(new BoardData("청산별곡4","박지원","1621.03.21","조회수 23 댓글 2"));
+        datas.add(new BoardData("ㅇㅇ","박지원","1621.03.21","조회수 23 댓글 2"));
         datas.add(new BoardData("청산별곡5","박지원","1621.03.21","조회수 23 댓글 2"));
         datas.add(new BoardData("청산별곡6","박지원","1621.03.21","조회수 23 댓글 2"));
 
