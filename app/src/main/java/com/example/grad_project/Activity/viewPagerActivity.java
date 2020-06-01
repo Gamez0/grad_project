f8a640538477911767b696e76222447e484b9b93
dobin <dobinshin@gmail.com>
2020-06-01 22:16:37 +0900

    carditem 구분할 수 있는 int 변수 추가

--- app/src/main/java/com/example/grad_project/Activity/viewPagerActivity.java
+++ app/src/main/java/com/example/grad_project/Activity/viewPagerActivity.java
@@ -67,10 +67,10 @@ public class viewPagerActivity extends AppCompatActivity implements View.OnClick
         });
 
         mCardAdapter = new CardPagerAdapter();
-        mCardAdapter.addCardItem(new CardItem(R.string.title_1, R.string.text_1));
-        mCardAdapter.addCardItem(new CardItem(R.string.title_2, R.string.text_1));
-        mCardAdapter.addCardItem(new CardItem(R.string.title_3, R.string.text_1));
-        mCardAdapter.addCardItem(new CardItem(R.string.title_4, R.string.text_1));
+        mCardAdapter.addCardItem(new CardItem(R.string.title_1, R.string.text_1,0));
+        mCardAdapter.addCardItem(new CardItem(R.string.title_2, R.string.text_1,1));
+        mCardAdapter.addCardItem(new CardItem(R.string.title_3, R.string.text_1,2));
+        mCardAdapter.addCardItem(new CardItem(R.string.title_4, R.string.text_1,3));
         mFragmentCardAdapter = new CardFragmentPagerAdapter(getSupportFragmentManager(),
                 dpToPixels(2, this));
 
--- app/src/main/java/com/example/grad_project/Item/CardItem.java
+++ app/src/main/java/com/example/grad_project/Item/CardItem.java
@@ -3,12 +3,14 @@ package com.example.grad_project.Item;
 public class CardItem {
     private int mTextResource;
     private int mTitleResource;
+    private int musicNum;
 
-    public CardItem(int title, int text) {
+    public CardItem(int title, int text, int num) {
         mTitleResource = title;
         mTextResource = text;
+        musicNum = num;
     }
-
+    public int getMusicNum(){ return musicNum; }
     public int getText() {
         return mTextResource;
     }
